2d62d9efa39ed2934bb7039b35825bd22152d942
Razvan Bugoi <razbugoi@gmail.com>
2022-10-04 16:29:26 +0100

    added delete button for each note

--- frontend/src/components/note/Note.js
+++ frontend/src/components/note/Note.js
@@ -15,7 +15,28 @@ const Note = (props) => {
       month: "short",
       weekday: "long",
       day: "numeric",
-      }); 
+      })
+
+      const deleteFunction = async (event) => {
+        event.preventDefault();
+        fetch( '/notes', {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${props.token}`
+          },
+          body: JSON.stringify({ _id: props.note._id })
+        })
+          .then(response => {
+            if(response.status === 201) {
+              console.log('OK')
+            } else {
+              alert('oops something is wrong')
+            }
+          }).then(() => {props.counterChanger(prevState => ({count: prevState.counter + 1}))})
+      }
+
+      const deleteBtnAppears = (() => {if (props.note.noteAuthor===props.userId) {return <button onClick= {deleteFunction} className={"like"} id={"deleteBtn"} title={"Delete note"}>Delete note</button>}})
 
 return(
       <article data-cy="note" className='note' key={ props.note._id }>
@@ -31,6 +52,8 @@ return(
         <div className="container-4">
           <p className='message con-4-1'>{ props.note.noteContent }</p>
        </div>
+
+       {deleteBtnAppears()}
 
       </article>
 )
